cc7a6d72448bf75ff93ba2e4c78b1c1fe3f54316
Pavel Pavlukovets <pavel0krutoi@gmail.com>
2022-08-17 06:34:29 +0300

    My fifth Swift lesson

---
+++ Fifth_lesson.swift
@@ -0,0 +1,52 @@
+// Lesson 5
+
+// First program
+//
+// for i in 0...10
+// {
+//     if  i == 5
+//     {
+//         break
+//     }
+//     print(i)
+// }
+
+// Second program
+//
+// var i = 10
+// while i >= 0
+// {
+//     print(i)
+//     i -= 1
+//     if i == 2
+//     {
+//         break
+//     }
+// }
+
+// Third program
+//
+// for i in 0...10
+// {
+//     if  i == 5
+//     {
+//         continue
+//     }
+//     print(i)
+// }
+
+// Fourth program
+//
+// var beg: Int8 = 1000
+// var ans: Int8 = 993
+// repeat
+// {
+//     print("\(beg) - 7 = \(ans).")
+//     beg -= 7
+//     ans -= 7
+//     if ans == 90
+//     {
+//         print("I'm Dead Inside.")
+//         continue
+//     }
+// } while ans > 5
