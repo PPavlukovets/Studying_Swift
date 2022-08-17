97784727bb2c6a6032d0c38584e6be536d8cb91b
Pavel Pavlukovets <pavel0krutoi@gmail.com>
2022-08-17 06:32:49 +0300

    My third Swift lesson

---
+++ Third_lesson.swift
@@ -0,0 +1,28 @@
+// Lesson 3
+
+// First program
+//
+// for item in 1...5
+//     {
+//     print(item)
+//     }
+// for v in 0...15 where v % 2 != 0 
+//     {
+//     print(v)
+// }
+
+// Second program
+//
+// var i = 10
+// while i != 5 
+//    {
+//     i -= 1
+//     } 
+// print(i," - it's true.")
+// var i = 10
+// repeat {
+//
+//    print(10 - i)
+//    i -= 1
+//    
+// } while i > 0
